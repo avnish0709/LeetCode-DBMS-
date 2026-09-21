2c46f871c546d71d9e915934f7d7473ae059e5d1
Avnish Zanjal <130134557+avnish0709@users.noreply.github.com>
2026-09-21 15:09:51 +0530

    Time: 728 ms (87.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,7 @@
+select product_name, sum(unit) unit
+from Products p 
+join Orders o
+on p.product_id = o.product_id
+where year(order_date) = 2020 and month(order_date) = 2
+group by p.product_name
+having sum(unit) > 99
